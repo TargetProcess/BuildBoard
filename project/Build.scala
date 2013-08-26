ddd4ba33eda58c425e3034bf63a05a1f709bbb02
Alex Fomin <Alex.Fomin@targetprocess.com>
2013-08-26 10:59:23 +0300

    Add GitHub lib

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object ApplicationBuild extends Build {
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    libraryDependencies += "org.kohsuke" % "github-api" % "1.43"
   )
 
 }
