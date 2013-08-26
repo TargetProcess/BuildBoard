dfbfed26aba099c9549791dc0f356c6646764398
Alex Fomin <Alex.Fomin@targetprocess.com>
2013-08-26 17:02:42 +0300

    Github login

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,10 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  
-  def index(message:String) = Action {
-    Ok(views.html.index(message))
+
+  def index(message: String) = Action {
+    implicit request =>
+      Ok(views.html.index(message))
   }
-  
+
 }
--- app/controllers/Login.scala
+++ app/controllers/Login.scala
@@ -1,21 +1,41 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
-import scalaj.http.{Http, Token}
+import play.api.mvc.{Cookie, Action, Controller}
+import scalaj.http.{HttpOptions, Http, Token}
 
 object Login extends Controller {
   val consumer = Token("72b7f38d644d0a1330f7", "dec66c9b3f49f0b5eba70fd163de03d5d76ce220")
 
 
-  def index = Action { implicit request =>
-    Ok(views.html.login(consumer.key))
+  def index = Action {
+    implicit request =>
+      Ok(views.html.login(consumer.key))
   }
 
   def oauth(code: String) = Action {
-    implicit request =>
 
-      val accessToken = Http("http://github.com/oauth/access_token").proxy("http://fomin", 8888).oauth(consumer,None,Some(code)).asString
 
-      Redirect(controllers.routes.Application.index(accessToken))
+    val req = Http.post("https://github.com/login/oauth/access_token")
+      .params(
+      "code" -> code,
+      "client_id" -> consumer.key,
+      "client_secret" -> consumer.secret
+    )
+      .header("Accept", "application/xml")
+      .option(HttpOptions.connTimeout(1000))
+      .option(HttpOptions.readTimeout(5000))
+
+
+    val accessTokenXml = req
+      .asXml
+
+
+
+
+    val clientCode = accessTokenXml \ "access_token"
+
+    Ok(views.html.closeWindow())
+      .withSession("github" -> clientCode.text)
+      .withCookies(Cookie("github", "token.ready"))
   }
 }
