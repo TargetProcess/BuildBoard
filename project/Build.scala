ae6c6b21f75ac3750d56865264ff3b9f05d1a643
Alex Fomin <Alex.Fomin@targetprocess.com>
2013-08-27 13:52:40 +0300

    Add mongoDb reference

--- project/Build.scala
+++ project/Build.scala
@@ -9,17 +9,15 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     // Add your project dependencies here,
-    jdbc,
-    anorm
-  )
-
+    anorm,
+    "se.radley" %% "play-plugins-salat" % "1.3.0")
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     libraryDependencies ++= Seq("org.kohsuke" % "github-api" % "1.43",
       "junit" % "junit" % "4.10" % "test",
       "org.scalaj" %% "scalaj-http" % "0.3.9",
-      "com.offbytwo.jenkins" % "jenkins-client" % "0.1.4"
-    )
-  )
+      "com.offbytwo.jenkins" % "jenkins-client" % "0.1.4"),
+    routesImport += "se.radley.plugin.salat.Binders._",
+    templatesImport += "org.bson.types.ObjectId")
 
 }
