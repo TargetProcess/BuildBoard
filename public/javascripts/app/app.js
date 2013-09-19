6b960c5147c0c099a368dba6a72b5d28ca65b510
Alex Fomin <ShurickFomin@gmail.com>
2013-09-20 00:31:54 +0300

    Pull request statuses

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -1,10 +1,28 @@
 angular.module('BuildBoard', ['ui.bootstrap']);
 
-var BranchesController = function($scope, $http){
+var BranchesController = function ($scope, $http, $window) {
 
-    $http.get(jsRoutes.controllers.Application.branches().absoluteURL()).success(function(data){
+    $http.get($window.jsRoutes.controllers.Application.branches().absoluteURL()).success(function (data) {
         $scope.branches = data;
     });
 
 };
 
+
+var PullRequestController = function ($scope, $http, $window) {
+    $scope.getClass = function (prStatus) {
+        if (prStatus.isMerged) {
+            return 'btn-primary';
+        } else if (prStatus.isMergeable) {
+            return 'btn-success';
+        } else {
+            return 'btn-danger';
+        }
+    };
+
+    if ($scope.branch.pullRequest) {
+        $http.get($window.jsRoutes.controllers.Github.pullRequestStatus($scope.branch.pullRequest.id).absoluteURL()).success(function (data) {
+            $scope.pullRequestStatus = data;
+        });
+    }
+};
