ee951fce1c3481ec66089e6253fa74fda70e4bac
Alex Fomin <Alex.Fomin@targetprocess.com>
2013-12-06 10:17:48 +0300

    fix presetup

--- presetup.cmd
+++ presetup.cmd
@@ -2,3 +2,4 @@ call npm install grunt --save-dev
 call npm install grunt-contrib-watch --save-dev
 call npm install grunt-typescript --save-dev
 call npm install -g grunt-cli
+call npm install -g typescript
