3a98db142f3e2b01f1b54e4887b1908b1f2a23c0
Alex Fomin <ShurickFomin@gmail.com>
2014-10-07 17:40:44 +0300

    Add gravatar icons for commits

--- app/assets/javascripts/controllers/BranchController.ts
+++ app/assets/javascripts/controllers/BranchController.ts
@@ -7,6 +7,7 @@ module buildBoard {
         getActivity(): ActivityEntry[];
         getBranch():Branch;
         getBuildStatus(build:Build):Status;
+        getGravatar(email:string):string;
     }
 
     export class BranchController {
@@ -23,6 +24,8 @@ module buildBoard {
             this.$scope.branchName = $state.params['name'];
             this.$scope.closeView = ()=> $state.go("list");
             this.$scope.getBuildStatus = StatusHelper.parse;
+
+            this.$scope.getGravatar = _.memoize((email:string)=> email ? md5(email.toLowerCase().trim()) : '0');
 
             modelProvider.getBranchWithActivities(this.$scope.branchName)
                 .then(branch => {
