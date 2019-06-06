16930cd81ef385d3c1a5727bef6ec34df254a36f
Dmitry Tsarevich <tsarevich@targetprocess.com>
2019-06-06 14:58:10 +0300

    More logging for Github/Jenkins subscriptions

--- app/models/services/CacheService.scala
+++ app/models/services/CacheService.scala
@@ -35,9 +35,11 @@ object CacheService {
   }
 
   def subscribeToJenkins: Subscription = {
+    play.Logger.info("Starting Jenkins subscription")
+
     val buildObservable = registry.buildWatcher.start
 
-    play.Logger.info("Start subscription")
+    play.Logger.info("Started Jenkins subscription")
 
     buildObservable
       .buffer(jenkinsInterval)
@@ -47,7 +49,7 @@ object CacheService {
           updateBuilds(buildNames)
         }        
         catch {
-          case e : Throwable => play.Logger.error("Error in jenkinsSubscription", e)
+          case e : Throwable => play.Logger.error("Error in Jenkins subscriptino", e)
         }
       )
   }
@@ -58,18 +60,24 @@ object CacheService {
 
     val updatedBuilds: Stream[Build] = registry.jenkinsService.getUpdatedBuilds(existingBuilds, updatedBuildNames)
 
+    play.Logger.info("got updated builds")
+
+    var updatedBuildsNum = 0
+
     for (updatedBuild <- updatedBuilds) {
         try {
           registry.buildRepository.update(updatedBuild)
           registry.buildRerun.rerunFailedParts(updatedBuild)
           registry.jobRunRepository.removeOld(DateTime.now().minusDays(7))
+
+          updatedBuildsNum += 1
         }
         catch {
           case e : Throwable => play.Logger.error("Error in Update builds", e)
         }
     }
 
-    play.Logger.info("builds were updated, notifying")
+    play.Logger.info(s"${updatedBuildsNum} builds were updated, notifying")
 
     registry.notificationService.notifyAboutBuilds(updatedBuilds.toIterator)
   }
@@ -96,10 +104,10 @@ object CacheService {
             data.foreach(branch => registry.branchRepository.update(branch))
           }
 
-        case Failure(e) => play.Logger.error("Error when get branches from github", e)
+        case Failure(e) => play.Logger.error("Error when get branches from Github", e)
       },
         error => {
-          play.Logger.error("Error in githubSubscription", error)
+          play.Logger.error("Error in Github subscription", error)
         })
   }
 }
